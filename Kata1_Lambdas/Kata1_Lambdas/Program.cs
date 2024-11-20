4e8d5b27ff9c888f38ac837d8162672c9f027a44
Svetlana Stepanovic Saine <svetlana.stepanovic.saine@student.forsbergsskola.se>
2024-11-20 18:31:58 +0100

    In Progress

--- Kata1_Lambdas/Kata1_Lambdas/Program.cs
+++ Kata1_Lambdas/Kata1_Lambdas/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var warrior = new Character("Warrior",30,() => Console.WriteLine("Warrior attacks with a sword!"));
+        var healer = new Character("Healer",60,() => Console.WriteLine("Healer heals!"));
+        
+        List<Character> characters = new List<Character> { warrior, healer };
+        
+        if (warrior.Health < 50)
+        {
+            warrior.PerformAction();
+        }
+        
+        if (healer.Health >= 50)
+        {
+            var characterToHeal = characters.Where(c => c.Health < 50).OrderBy(c => c.Health).FirstOrDefault();
+            if (characterToHeal != null)
+            {
+                Console.WriteLine($"{healer.Name} heals {characterToHeal.Name}.");
+                characterToHeal.Health += 20;
+                healer.PerformAction();
+            }
+            else
+            {
+                Console.WriteLine("Healer has no one to heal!");
+            }
+        }
+        characters.Where(c => c.Health < 50).ToList().ForEach(c =>
+            {
+                if (c.Name == "Warrior")
+                {
+                    c.PerformAction();
+                }
+                else if (c.Name == "Healer")
+                {
+                    c.PerformAction();
+                }
+            
+            }
+        );
+        
     }
 }
